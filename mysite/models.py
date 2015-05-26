d411838404b2aef8625aef61338cad747399c1ee
Mr.orange <309871271@qq.com>
2015-05-26 23:27:47 +0800

    doc

--- mysite/models.py
+++ mysite/models.py
@@ -238,26 +238,3 @@ class WikiType(models.Model):
     class Meta:
         db_table = 'wiki_type'
 
-
-class Wiki(models.Model):
-    """
-    ---------------------------------------
-    功能说明：Wiki数据模型
-    ---------------------------------------
-    时间:    2015－04－19
-    ---------------------------------------
-    """
-    category = models.IntegerField()
-    content = wmd_models.MarkDownField()
-    content_show = wmd_models.MarkDownField(u'show', null=True)
-    add_time = models.DateTimeField(auto_now=True, auto_now_add=True)
-
-    class Meta:
-        db_table = 'wiki'
-
-    def save(self, force_insert=False, force_update=False, using=None):
-        self.content_show = mark_safe(markdown.markdown(force_unicode(self.content), ['codehilite'], safe_mode='escape'))
-        super(Wiki, self).save()
-
-    class Meta:
-        db_table = 'wiki_type'
